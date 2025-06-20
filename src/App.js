a61789119a0101a7c46d7eca78464b707f2b597b
Lenise Moraes <lemoraes.santana@gmail.com>
2025-06-20 12:49:38 -0300

    Added the component form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-logo img-fluid" alt="SheCodes logo" />
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="SheCodes logo" />
+      </header>
+      <Dictionary />
+      <footer>Coded by Lenise Moraes</footer>
     </div>
   );
 }
