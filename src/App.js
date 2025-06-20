6d7f0c26283c3905351cf6869639ca3f46de6e66
Lenise Moraes <lemoraes.santana@gmail.com>
2025-06-20 11:29:52 -0300

    Start the repository and add the shecodes logo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} className="App-logo img-fluid" alt="SheCodes logo" />
+    </div>
+  );
+}
+
+export default App;
