f41908ed4f41fa555cd650aaac4e51b86339735e
Lenise Moraes <lemoraes.santana@gmail.com>
2025-06-21 14:51:39 -0300

    Added axios and the api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function getResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiKey = "5btac2eb2ed52b236021789ofe9a3348";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=sunset&key=${apiKey}`;
+    axios.get(apiUrl).then(getResponse);
   }
 
   function changeKeyword(event) {
