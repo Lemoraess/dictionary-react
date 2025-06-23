cfa0543e2e83172c2cde38995096501470254900
Lenise Moraes <lemoraes.santana@gmail.com>
2025-06-23 13:17:37 -0300

    Added the phonetic line

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,7 +5,7 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <h5>{props.meaning.partOfSpeech}</h5>
       <p>Definition: {props.meaning.definition}</p>
       <em>Example: {props.meaning.example}</em>
 
