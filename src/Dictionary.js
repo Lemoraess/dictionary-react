1f584b8f25586e48c3324ed8f9a2efcf8db3844d
Lenise Moraes <lemoraes.santana@gmail.com>
2025-06-23 17:41:21 -0300

    Added images component and final adjustments

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,30 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Images from "./Images";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultkeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [images, setImages] = useState(null);
 
   function getResponse(response) {
     setResults(response.data);
+  }
+
+  function getImages(response) {
+    setImages(response.data);
   }
 
   function search() {
     let apiKey = "5btac2eb2ed52b236021789ofe9a3348";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(getResponse);
+
+    let imgApiKey = "5btac2eb2ed52b236021789ofe9a3348";
+    let imgApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imgApiKey}`;
+    axios.get(imgApiUrl).then(getImages);
   }
 
   function handleSubmit(event) {
@@ -51,6 +61,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Images images={images} />
       </div>
     );
   } else {
